b2b60a07b1cdaf7ab7e1e39699fd321dfaa30066
BleskoDev <bleskodev@gmail.com>
2016-04-02 22:34:47 +0200

    Add save function to noProvider class.
    It saves containing metadata info as a local freedb record.

--- spec/metadata/noProvider_spec.rb
+++ spec/metadata/noProvider_spec.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 #    Rubyripper - A secure ripper for Linux/BSD/OSX
 #    Copyright (C) 2007 - 2011 Bouke Woudstra (boukewoudstra@gmail.com)
+#    Copyright (C) 2016 BleskoDev (bleskodev@gmail.com)
 #
 #    This file is part of Rubyripper. Rubyripper is free software: you can
 #    redistribute it and/or modify it under the terms of the GNU General
@@ -20,13 +21,16 @@ require 'rubyripper/metadata/noProvider'
 describe NoProvider do
  
   let(:disc) {double('Disc').as_null_object}
+  let(:md) {double('Metadata::Data').as_null_object}
+  let(:generator) {double('FreedbRecordGenerator').as_null_object}
+  let(:save) {double('SaveFreedbRecord').as_null_object}
   let(:md_empty) {Metadata::Data.new()}
-  let(:no_provider) {NoProvider.new(disc)}
 
   context "When the metadata for a disc is requested" do
     it "should fill metadata tracklist with default track names" do
       nb_tracks = 5
       allow(disc).to receive(:audiotracks).and_return(nb_tracks)
+      no_provider = NoProvider.new(disc, nil, nil, save)
       no_provider.get()
       expect(no_provider.status).to eq('ok')
       expect(no_provider.tracklist.length).to eq(nb_tracks)
@@ -35,5 +39,21 @@ describe NoProvider do
       end
     end
   end
+
+  context "When save is requested" do
+    it "should generate freedb record and save it" do
+      freedbRecord = 'fake record'
+      category = 'Jazz'
+      discid = 'a56b3400'
+      expect(generator).to receive(:generate).with(disc).and_return(freedbRecord)
+      expect(save).to receive(:save).with(freedbRecord, category, discid)
+      expect(md).to receive(:genre).and_return(category)
+      expect(disc).to receive(:freedbDiscid).and_return(discid)
+
+      no_provider = NoProvider.new(disc, md, generator, save)
+      no_provider.save()
+    end
+  end
+
 end
 
